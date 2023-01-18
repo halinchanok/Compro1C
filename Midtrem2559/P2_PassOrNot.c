64091a224cc7db9dcf07b21b29af610c6fd77d9b
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-01-18 21:50:41 +0700

    Finish

--- Midtrem2559/P2_PassOrNot.c
+++ Midtrem2559/P2_PassOrNot.c
@@ -4,17 +4,27 @@ void main(){
     scanf("%d%d%d%d", &score_m1, &score_m2, &score_m3, &score_m4);
     int score1, score2, score3, score4;
     scanf("%d%d%d%d", &score1, &score2, &score3, &score4);
+    int count = 0;
     if(score1>=score_m1){
         printf("1 ");
+        count++;
     }
     if(score2>=score_m2){
         printf("2 ");
+        count++;
     }
     if(score3>=score_m3){
         printf("3 ");
+        count++;
     }
     if(score4>=score_m4){
         printf("4 ");
+        count++;
     }
-
+    if(count == 0){
+        printf("0");
+    }
+    if(count>2){
+        printf("\npass");
+    }else printf("\nfail");
 }
