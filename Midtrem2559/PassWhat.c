7ed2fbe1807c1b50cdf0aada328bdef57866d0df
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-01-18 21:34:48 +0700

    Finish

---
+++ Midtrem2559/PassWhat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+    int score_m1, score_m2, score_m3, score_m4;
+    scanf("%d%d%d%d", &score_m1, &score_m2, &score_m3, &score_m4);
+    int score1, score2, score3, score4;
+    scanf("%d%d%d%d", &score1, &score2, &score3, &score4);
+    if(score1>=score_m1){
+        printf("1 ");
+    }
+    if(score2>=score_m2){
+        printf("2 ");
+    }
+    if(score3>=score_m3){
+        printf("3 ");
+    }
+    if(score4>=score_m4){
+        printf("4 ");
+    }
+}
