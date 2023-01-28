2ca6b5d2dd75acf3ba23ddffb29edf9388f4557e
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-01-28 12:19:00 +0700

    Finish

--- NestedLoop/W8P2_RowCounting.c
+++ NestedLoop/W8P2_RowCounting.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 void main(){
-    int R, C;
+    int R, C, count=1;
     scanf("%d%d", &R, &C);
     for(int i=0; i<R; i++){
-        for(int j=0; j<C;)
+        for(int j=0; j<C; j++){
+            printf("%d ", count);
+            count++;
+        }printf("\n");
     }
 }
