9071814bfa8497bc15e72dbd73d794b39835007a
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-02-16 10:39:27 +0700

    Finish

--- 2DArray/W10P4_CarParkMap.c
+++ 2DArray/W10P4_CarParkMap.c
@@ -1,32 +1,26 @@
 #include<stdio.h>
 void main(){
-    int R, C;
-    scanf("%d%d%d", &R, &C);
-    int arr[R][C];
-    for(int i=1; i<=R; i++){
-        for(int j=1; j<=C; j++){
+    int M, N;
+    scanf("%d%d", &M,&N);
+    int arr[M][N];
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
             arr[i][j] = 0;
         }
     }
-    
     int K;
     scanf("%d", &K);
     for(int i=0; i<K; i++){
-        int M, N;
-        scanf("%d%d", &M, &N);
-        arr[M][N] = 1;
+        int R, C;
+        scanf("%d%d", &R,&C);
+        arr[R-1][C-1]=1;
     }
-    for(int i=1; i<=R; i++){
-        for(int j=1; j<=C; j++){
-            printf("%d ", arr[i][j]);
-        }printf("\n");
-    }
-    for(int i=1; i<=R; i++){
-        for(int j=1; j<=C; j++){
-            if(arr[i][j]==0){
-                printf("_");
-            }else if(arr[i][j]==1){
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
+            if(arr[i][j]==1){
                 printf("x");
+            }else if(arr[i][j]==0){
+                printf("_");
             }
         }printf("\n");
     }
