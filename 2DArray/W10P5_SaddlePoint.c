e228e12f36db02114c4bd18fdde0a75bd04e5a7d
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-02-16 11:31:05 +0700

    Finish

--- 2DArray/W10P5_SaddlePoint.c
+++ 2DArray/W10P5_SaddlePoint.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<limits.h>
 void main(){
-    int R , C;
+    int R, C;
     scanf("%d%d", &R, &C);
     int arr[R][C];
     for(int i=0; i<R; i++){
@@ -9,39 +9,48 @@ void main(){
             scanf("%d", &arr[i][j]);
         }
     }
-    int c_Min, c_Max, r_Min, r_Max;
-    int s1 = 0, s2 = 0;
+    int count = 0 ;
     for(int i=0; i<R; i++){
         int max = INT_MIN;
         int min = INT_MAX;
         for(int j=0; j<C; j++){
-            if(arr[i][j] > max){
-                max = arr[i][j];
-                c_Min = j;
-                r_Min = i;
+            if(arr[i][j]>max){
+                max=arr[i][j];
             }
-            if(arr[i][j] < min){
-                min = arr[i][j];
-                c_Max = j;
-                r_Max = i;
+            if(arr[i][j]<min){
+                min=arr[i][j];
             }
         }
-        for(int j=0; j<)
-        int ck1 = 0, ck2 =0;
-        for(int k=0; k<R; k++){
-            if(arr[c_Min][k] > max) 
-                ck1++;
+        for(int j=0; j<C; j++){
+            if(arr[i][j]==max){
+                int ck = 0;
+                for(int k=0; k<R; k++){
+                    if(arr[i][j] > arr[k][j]){
+                        ck++;
+                    }
+                }
+                if(ck==0){
+                    printf("(%d, %d) = %d\n", i, j, arr[i][j]);
+                    count++;
+                }
+            }
         }
-        if(ck1==R-1) 
-            s1 = max;
-
-        for(int k=0; k<R; k++){
-            if(arr[c_Max][k] < min) 
-                ck2++;
+        for(int j=0; j<C; j++){
+            if(arr[i][j]==min){
+                int ck = 0;
+                for(int k=0; k<R; k++){
+                    if(arr[i][j] < arr[k][j]){
+                        ck++;
+                    }
+                }
+                if(ck==0){
+                    printf("(%d, %d) = %d\n", i, j, arr[i][j]);
+                    count++;
+                }
+            }
         }
-        if(ck2==R-1) 
-            s2 = min;
     }
-    printf("(%d, %d) = %d\n", r_Min, c_Min, s1);
-    printf("(%d, %d) = %d", r_Max, c_Max, s2);
+    if(count==0){
+        printf("None");
+    }
 }
